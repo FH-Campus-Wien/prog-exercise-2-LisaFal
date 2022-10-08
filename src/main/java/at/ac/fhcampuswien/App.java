8aa01ce6f9c9dd44ae1ec443711e4cc5e6dad83c
LisaFal <lisa.falter@hotmail.com>
2022-10-09 00:33:51 +0200

    Task 1 and 2 completed

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -1,18 +1,82 @@
 package at.ac.fhcampuswien;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class App {
 
     //todo Task 1
-    public void largestNumber(){
+    public void largestNumber() {
         // input your solution here
+
+        double number;
+        double numbermax = 0;
+        int count;
+
+        System.out.print("Number 1: ");
+        Scanner scanni = new Scanner(System.in);
+        number = scanni.nextDouble();
+
+        if (number <= 0) {
+            System.out.println("No number entered.");
+            numbermax = number;
+
+        } else {
+            for (count = 2; number > 0; count += 1) {
+                System.out.print("Number " + count + ": ");
+                number = scanni.nextDouble();
+                if (number > numbermax) {
+                    numbermax = number;
+                }
+            }  // formatting a decimal: /source: https://forum.chip.de/discussion/400137/java-bei-double-nur-2-nachkommastellen
+            DecimalFormat f = new DecimalFormat("#0.00");
+            System.out.println("The largest number is " + f.format(numbermax));
+        }
+
     }
+
 
     //todo Task 2
     public void stairs(){
+        int numberOfLines;
+        int sum = 0;
+
+        System.out.print("n: ");
+        Scanner scan1 = new Scanner(System.in);
+        numberOfLines = scan1.nextInt();
+
+        if (numberOfLines <= 0) {
+            System.out.println("Invalid number!");
+
+        } else {
+            for(int row = 0; row < numberOfLines; row++) {
+
+                for(int i = 0; i <= row; i++) {
+                    sum = sum + 1;
+
+                    System.out.print(sum + " ");
+
+                }
+
+                System.out.println();
+
+            }
+
+          }
+
+
+
+      }
+
+
+
         // input your solution here
-    }
+
+
+
+
+
+
 
     //todo Task 3
     public void printPyramid(){
@@ -27,6 +91,21 @@ public class App {
     //todo Task 5
     public void marks(){
         // input your solution here
+        int mark;
+        Scanner scanner = new Scanner(System.in);
+        int count = 0;
+
+        do {
+            System.out.println("Mark " + count++);
+            mark = scanner.nextInt();
+        } while (mark >= 1 && mark <= 5);
+        {
+
+        }
+
+
+
+
     }
 
     //todo Task 6
